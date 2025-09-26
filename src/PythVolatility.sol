585018a6828f683fd9317ab9bf926a5b7e5eba1c
saurabh <saurabhyadav42399@gmail.com>
2025-09-26 23:47:11 +0530

    added empty pyth Lib

--- src/PythVolatility.sol
+++ src/PythVolatility.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-library PythVolatilityLib {}
+library PythVolatilityLib {
+    // struct VolatilityStorge {
+    //       uint256 volatility;
+    // }
+}
