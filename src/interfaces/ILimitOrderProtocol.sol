bea8058e7f5e0e210f4dbff1afd6775f33e4b092
saurabh <saurabhyadav42399@gmail.com>
2025-09-26 22:32:56 +0530

    added interface for 1inch

---
+++ src/interfaces/ILimitOrderProtocol.sol
@@ -0,0 +1,29 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.20;
+
+// import {AddressLib, Address} from "../src/libraries/AddressLib.sol";
+// import {MakerTraits, MakerTraitsLib} from "../src/libraries/MakerTraitLib.sol";
+
+// interface ILimitOrderProtocol {
+//     struct Order {
+//         uint256 salt;
+//         Address maker;
+//         Address receiver;
+//         Address makerAsset;
+//         Address takerAsset;
+//         uint256 makingAmount;
+//         uint256 takingAmount;
+//         MakerTraits makerTraits;
+//     }
+
+//     function fillOrderArgs(
+//         Order calldata order,
+//         bytes32 r,
+//         bytes32 vs,
+//         uint256 amount,
+//         uint256 takerTraits,
+//         bytes calldata fillOrderArgs
+//     ) external payable returns (uint256, uint256, bytes32);
+
+//     function hashOrder(Order calldata order) external view returns (bytes32);
+// }
