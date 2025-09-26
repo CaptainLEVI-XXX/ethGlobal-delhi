edff33651fab87dae21bb1ca76ef047ab0e5f0c3
saurabh <saurabhyadav42399@gmail.com>
2025-09-26 23:35:14 +0530

    feat: added project set up for 1inch limit order

--- src/1InchDynamicOrder.sol
+++ src/1InchDynamicOrder.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IAmountGetter} from "./interfaces/IAmountGetter.sol";
+import {IOrderMixin, IAmountGetter} from "./interfaces/IAmountGetter.sol";
+import {CustomRevert} from "v4-core/libraries/CustomRevert.sol";
+import {AddressLib, Address} from "./libraries/AddressLib.sol";
+import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
+import {PythVolatilityLib} from "./PythVolatility.sol";
 
-
-contract OneInchDynamicOrder is IAmountGetter {
-
-    
-
-
-}
+abstract contract OneInchDynamicOrder is IAmountGetter {}
